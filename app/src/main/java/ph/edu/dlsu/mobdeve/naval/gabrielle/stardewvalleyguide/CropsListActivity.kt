e3d7f46e21df9ef6c7be79f7775160e90e527e81
gabriellekaye <gabrielle_naval@dlsu.edu.ph>
2022-05-11 15:38:18 +0800

    added crop list page + ui

--- app/src/main/java/ph/edu/dlsu/mobdeve/naval/gabrielle/stardewvalleyguide/CropsListActivity.kt
+++ app/src/main/java/ph/edu/dlsu/mobdeve/naval/gabrielle/stardewvalleyguide/CropsListActivity.kt
@@ -2,17 +2,15 @@ package ph.edu.dlsu.mobdeve.naval.gabrielle.stardewvalleyguide
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import ph.edu.dlsu.mobdeve.naval.gabrielle.stardewvalleyguide.databinding.ActivityHomeBinding
+import ph.edu.dlsu.mobdeve.naval.gabrielle.stardewvalleyguide.databinding.ActivityCropsListBinding
 
-class HomeActivity : AppCompatActivity() {
+class CropsListActivity : AppCompatActivity() {
 
-    private lateinit var binding : ActivityHomeBinding
+    private lateinit var binding : ActivityCropsListBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityHomeBinding.inflate(layoutInflater)
+        binding = ActivityCropsListBinding.inflate(layoutInflater)
         setContentView(binding.root)
     }
-
-
 }
