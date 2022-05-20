72e9ca7071b4bb543c9729bc9f6f065cfe9036c0
gabriellekaye <gabrielle_naval@dlsu.edu.ph>
2022-05-20 20:24:24 +0800

    edited logo sizes

--- app/src/main/java/ph/edu/dlsu/mobdeve/naval/gabrielle/stardewvalleyguide/SpringCropsListActivity.kt
+++ app/src/main/java/ph/edu/dlsu/mobdeve/naval/gabrielle/stardewvalleyguide/SpringCropsListActivity.kt
@@ -51,8 +51,30 @@ class SpringCropsListActivity : AppCompatActivity() {
         crop2.pierre = "30g"
         crop2.joja = "37g"
 
+        var crop3 = Crop()
+        crop3.name = "Coffee Bean"
+        crop3.season = "Spring"
+        crop3.img = R.drawable.coffeebean
+        crop3.growth = "10 days"
+        crop3.regrowth = "2 days"
+        crop3.sell = "15g - 30g"
+        crop3.pierre = "Not Sold Here"
+        crop3.joja = "Not Sold Here"
+
+        var crop4 = Crop()
+        crop4.name = "Garlic"
+        crop4.season = "Spring"
+        crop4.img = R.drawable.garlic
+        crop4.growth = "4 days"
+        crop4.regrowth = "No"
+        crop4.sell = "60g - 120g"
+        crop4.pierre = "40g"
+        crop4.joja = "Not Sold Here"
+
         dao.addCrop(crop1)
         dao.addCrop(crop2)
+        dao.addCrop(crop3)
+        dao.addCrop(crop4)
 
         cropArrayList = dao.getCrop()
     }
