cd6571e3931589713f99da991751fa87e5f8a6be
gretvm <80465081+gretvm@users.noreply.github.com>
2022-05-25 16:23:06 +0800

    Login and Register UI and Intent

--- app/src/main/java/ph/edu/dlsu/mobdeve/naval/gabrielle/stardewvalleyguide/RegisterActivity.kt
+++ app/src/main/java/ph/edu/dlsu/mobdeve/naval/gabrielle/stardewvalleyguide/RegisterActivity.kt
@@ -4,24 +4,20 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import ph.edu.dlsu.mobdeve.naval.gabrielle.stardewvalleyguide.databinding.ActivityMainBinding
+import ph.edu.dlsu.mobdeve.naval.gabrielle.stardewvalleyguide.databinding.ActivityRegisterBinding
 
-class MainActivity : AppCompatActivity() {
+class RegisterActivity : AppCompatActivity() {
 
-    private lateinit var binding : ActivityMainBinding
+    private lateinit var binding : ActivityRegisterBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.btnLogin.setOnClickListener {
+        binding.btnRegister.setOnClickListener{
             var goToHome = Intent(this, HomeActivity::class.java)
             startActivity(goToHome)
-        }
-
-        binding.tvRegister.setOnClickListener {
-            var goToRegister = Intent(this, RegisterActivity::class.java)
-            startActivity(goToRegister)
         }
     }
 }
