63047ae8413ac6db111c21157e36bc670a0fc519
gabriellekaye <gabrielle_naval@dlsu.edu.ph>
2022-05-28 12:59:57 +0800

    finished crops

--- app/src/main/java/ph/edu/dlsu/mobdeve/naval/gabrielle/stardewvalleyguide/SpringCropsListActivity.kt
+++ app/src/main/java/ph/edu/dlsu/mobdeve/naval/gabrielle/stardewvalleyguide/SpringCropsListActivity.kt
@@ -1,24 +1,23 @@
 package ph.edu.dlsu.mobdeve.naval.gabrielle.stardewvalleyguide
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.LinearLayoutManager
 import ph.edu.dlsu.mobdeve.naval.gabrielle.stardewvalleyguide.adapter.CropAdapter
 import ph.edu.dlsu.mobdeve.naval.gabrielle.stardewvalleyguide.dao.CropsDADArrayImpl
 import ph.edu.dlsu.mobdeve.naval.gabrielle.stardewvalleyguide.dao.CropsDAO
-import ph.edu.dlsu.mobdeve.naval.gabrielle.stardewvalleyguide.databinding.ActivitySpringCropsListBinding
+import ph.edu.dlsu.mobdeve.naval.gabrielle.stardewvalleyguide.databinding.ActivityCropListBinding
 import ph.edu.dlsu.mobdeve.naval.gabrielle.stardewvalleyguide.model.Crop
 
 class SpringCropsListActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivitySpringCropsListBinding
+    private lateinit var binding: ActivityCropListBinding
     private lateinit var cropAdapter: CropAdapter
     private lateinit var cropArrayList: ArrayList<Crop>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivitySpringCropsListBinding.inflate(layoutInflater)
+        binding = ActivityCropListBinding.inflate(layoutInflater)
         setContentView(binding.root)
         init()
 
@@ -71,10 +70,98 @@ class SpringCropsListActivity : AppCompatActivity() {
         crop4.pierre = "40g"
         crop4.joja = "Not Sold Here"
 
+        var crop5 = Crop()
+        crop5.name = "Green Bean"
+        crop5.season = "Spring"
+        crop5.img = R.drawable.greenbean
+        crop5.growth = "10 days"
+        crop5.regrowth = "3 days"
+        crop5.sell = "40g - 80g"
+        crop5.pierre = "60g"
+        crop5.joja = "75g"
+
+        var crop6 = Crop()
+        crop6.name = "Kale"
+        crop6.season = "Spring"
+        crop6.img = R.drawable.kale
+        crop6.growth = "6 days"
+        crop6.regrowth = "No"
+        crop6.sell = "100g - 220g"
+        crop6.pierre = "70g"
+        crop6.joja = "87g"
+
+        var crop7 = Crop()
+        crop7.name = "Parsnip"
+        crop7.season = "Spring"
+        crop7.img = R.drawable.parsnip
+        crop7.growth = "4 days"
+        crop7.regrowth = "No"
+        crop7.sell = "35g - 70g"
+        crop7.pierre = "20g"
+        crop7.joja = "25g"
+
+        var crop8 = Crop()
+        crop8.name = "Potato"
+        crop8.season = "Spring"
+        crop8.img = R.drawable.potato
+        crop8.growth = "6 days"
+        crop8.regrowth = "No"
+        crop8.sell = "80g - 160g"
+        crop8.pierre = "50g"
+        crop8.joja = "62g"
+
+        var crop9 = Crop()
+        crop9.name = "Rhubarb"
+        crop9.season = "Spring"
+        crop9.img = R.drawable.rhubarb
+        crop9.growth = "13 days"
+        crop9.regrowth = "No"
+        crop9.sell = "220g - 440g"
+        crop9.pierre = "Not Sold Here"
+        crop9.joja = "Not Sold Here"
+
+        var crop10 = Crop()
+        crop10.name = "Strawberry"
+        crop10.season = "Spring"
+        crop10.img = R.drawable.strawberry
+        crop10.growth = "8 days"
+        crop10.regrowth = "4 days"
+        crop10.sell = "120g - 240g"
+        crop10.pierre = "200g (Egg Festival)"
+        crop10.joja = "Not Sold Here"
+
+        var crop11 = Crop()
+        crop11.name = "Tulip"
+        crop11.season = "Spring"
+        crop11.img = R.drawable.tulip
+        crop11.growth = "6 days"
+        crop11.regrowth = "No"
+        crop11.sell = "30g - 60g"
+        crop11.pierre = "20g"
+        crop11.joja = "25g"
+
+        var crop12 = Crop()
+        crop12.name = "Unmilled Rice"
+        crop12.season = "Spring"
+        crop12.img = R.drawable.unmilledrice
+        crop12.growth = "8-6 days"
+        crop12.regrowth = "No"
+        crop12.sell = "30g - 60g"
+        crop12.pierre = "40g"
+        crop12.joja = "Not Sold Here"
+
         dao.addCrop(crop1)
         dao.addCrop(crop2)
         dao.addCrop(crop3)
         dao.addCrop(crop4)
+        dao.addCrop(crop5)
+        dao.addCrop(crop6)
+        dao.addCrop(crop7)
+        dao.addCrop(crop8)
+        dao.addCrop(crop9)
+        dao.addCrop(crop10)
+        dao.addCrop(crop11)
+        dao.addCrop(crop12)
 
         cropArrayList = dao.getCrop()
     }
